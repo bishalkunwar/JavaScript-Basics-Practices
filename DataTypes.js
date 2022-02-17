0e748d343a4d40fb1893a43144436d297766c444
bishalkunwar <bishal.kunwar487@gmail.com>
2022-02-17 19:53:17 +0545

    JS Data Types

---
+++ DataTypes.js
@@ -0,0 +1,33 @@
+// const myName = "Fuck you";
+// const age = 24;
+// const isNumber = true;
+
+
+// console.log(isNumber);
+// console.log(typeof(isNumber));
+
+// console.log(myName);
+// console.log(typeof(myName));
+
+// console.log(age);
+// console.log(typeof(age));
+
+// console.log(10 + "20");
+// console.log(9 - "5");
+// console.log("java" + "script");
+// console.log(" " + " ");
+// console.log(" " + " " + 0);
+// console.log("vinod" - "thapa");
+
+// console.log(true + true);
+// console.log(false - true);
+
+// let check;
+// console.log(check);
+// check = "bishal";
+// console.log(check);
+// check = "new name";
+// console.log(check);
+// console.log(typeof(check));
+
+console.log(Number.isNaN(NaN));
